efd09f564400c23085949e64ff0b435762114c1f
Prajwal77777 <pkascol664@gmail.com>
2023-06-29 22:03:25 +0545

    Outdated CS50

--- outdated.py
+++ outdated.py
@@ -17,18 +17,21 @@ while True:
     date = input("Date: ")
     try:
         month, days, year = date.split("/")
-        if (int(days >= 1) and int(days <= 31) and int(month >= 1) and int(month <= 12)):
+        if (int(days) >= 1 and int(days) <= 31 and int(month) >= 1 and int(month) <= 12):
             break
     except:
         try:
             O_months, O_days, O_year = date.split(" ")
             for i in range(len(months)):
                 if O_months == months[i]:
-                    month = i + 1
-            days = O_days.replace(",", "")
-            if (int(days >= 1) and int(days <= 31) and int(month >= 1) and int(month <= 12)):
+                    month = str(i + 1)
+                    break
+            else:
+                continue
+            days = O_days.rstrip(",")
+            if (int(days) >= 1 and int(days) <= 31 and int(month) >= 1 and int(month) <= 12):
                 break
         except:
-            print()
-            pass
-print(f"{year}-{int(month):02}-{int(days):02}")
+            print("Invalid date format. Please try again.")
+
+print(f"{year}-{month.zfill(2)}-{days.zfill(2)}")
