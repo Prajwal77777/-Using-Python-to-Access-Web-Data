2dcb325bc2abb642e4a465fb937e211180ad63e2
Prajwal77777 <pkascol664@gmail.com>
2023-07-17 13:43:35 +0545

    FreeCodeCamp test

--- CalculatorTest/budget.py
+++ CalculatorTest/budget.py
@@ -1,59 +1,57 @@
 class Category:
     def __init__(self, name):
         self.name = name
-        self.details = []
+        self.ledger = []
 
     def deposit(self, amount, description=""):
-        self.details.append({"amount": amount, "description": description})
+        self.ledger.append({"amount": amount, "description": description})
 
     def withdraw(self, amount, description=""):
-        if (self.check_fund(amount)):
-            self.details.append(
-                {"amount": -amount, "description": description})
+        if self.check_funds(amount):
+            self.ledger.append({"amount": -amount, "description": description})
             return True
         return False
 
     def get_balance(self):
         balance = 0
-        for transaction in self.details:
+        for transaction in self.ledger:
             balance += transaction["amount"]
         return balance
 
     def transfer(self, amount, category):
-        if self.check_fund(amount):
+        if self.check_funds(amount):
             self.withdraw(amount, f"Transfer to {category.name}")
-            category.deposit(amount, f"Transfer from{self.name}")
+            category.deposit(amount, f"Transfer from {self.name}")
             return True
         return False
 
-    def check_fund(self, amount):
+    def check_funds(self, amount):
         return amount <= self.get_balance()
+
+    def get_withdrawals(self):
+        withdrawals = 0
+        for transaction in self.ledger:
+            if transaction["amount"] < 0:
+                withdrawals -= transaction["amount"]
+        return withdrawals
 
     def __str__(self):
         title = f"{self.name:*^30}\n"
         items = ""
         total = 0
-
-        for transaction in self.details:
+        for transaction in self.ledger:
             items += f"{transaction['description'][:23]:23}" + \
                 f"{transaction['amount']:>7.2f}\n"
-            total += transaction["amount"]
-            output = title + items + f"Total:{total:.2f}"
-            return output
+            total += transaction['amount']
+        output = title + items + f"Total: {total:.2f}"
+        return output
 
 
 def create_spend_chart(categories):
-    total_withdrawals = 0
-    category_names = []
-    spent_percentages = []
-
-    for category in categories:
-        total_withdrawals += category.get_withdrawals()
-        category_names.append(category.name)
-
-    for category in categories:
-        spent_percentage = category.get_withdrawals() / total_withdrawals * 100
-        spent_percentages.append(spent_percentage)
+    total_withdrawals = sum(category.get_withdrawals()
+                            for category in categories)
+    spent_percentages = [(category.get_withdrawals() /
+                          total_withdrawals) * 100 for category in categories]
 
     chart = "Percentage spent by category\n"
     for percentage in range(100, -10, -10):
@@ -67,14 +65,15 @@ def create_spend_chart(categories):
 
     chart += "    " + "-" * (len(categories) * 3 + 1) + "\n"
 
-    max_name_length = max(len(name) for name in category_names)
+    max_name_length = max(len(category.name) for category in categories)
     for i in range(max_name_length):
-        chart += "    "
-        for name in category_names:
-            if i < len(name):
-                chart += name[i] + "  "
+        chart += "     "
+        for category in categories:
+            if i < len(category.name):
+                chart += category.name[i] + "  "
             else:
                 chart += "   "
+        chart = chart.rstrip() + "\n"  # Remove trailing whitespace from each line
         if i < max_name_length - 1:
             chart += "\n"
 
